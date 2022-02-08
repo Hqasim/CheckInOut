db25c8d447bdf9bcb31322ea12f472ffebc49132
hamzahqasim <hqqasim55@gmail.com>
2022-02-08 17:36:32 +0500

    Initial Auth0 Implement

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,19 @@
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import AuthenticationButton from './components/authentication-button';
+import Dashboard from './components/dashboard';
+
 function App() {
     return (
-        <div className="App">
-            <h1>Check In / Out</h1>
+        <div className='main-wrapper'>
+            <BrowserRouter>
+                <h1>Check In / Out</h1>
+                <Routes>
+                    <Route path='/dashboard' element={<Dashboard />} />
+                </Routes>
+                <AuthenticationButton />
+            </BrowserRouter>
         </div>
     );
 }
--- app/src/index.js
+++ app/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.css'; // Bootstrap
+import { Auth0Provider } from '@auth0/auth0-react' // Auth0
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Auth0Provider
+            domain="dev-f1w8u9n8.us.auth0.com"
+            clientId="7kKWTphZbl8PgNyTWXhXoY2ibdrrRLGy"
+            redirectUri={window.location.origin}>
+            <App />
+        </Auth0Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );
